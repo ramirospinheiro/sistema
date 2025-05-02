6e8c5ff2aadce967cc0b2f68a0583adf3fe984f5
ramirospinheiro <ramiropinheiro97@gmail.com>
2025-05-02 11:50:02 -0300

    login linkado finalizado

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,28 @@
+import { Link, useNavigate } from "react-router-dom"
+import svg from "/public/tablet-login-animate.svg"  
+
 export default function LoginPage(){
+    const navigate = useNavigate()
     return (
-        <p>Aqui é a pagina de LOGIN :)</p>
+        <div className="w-full h-screen flex">
+            <div className="w-[40%] h-full flex items-center justify-center ">
+                {/* metodo Link: pode incluir links externos */}
+                {/* <Link to="/cadastro">Cadastro</Link> */}
+
+                {/* metodo navigate: apenas paginas internas, é mais rapido mas é uma biblioteca, portanto melhor evitar */}
+                {/* <button onClick={() => navigate("/cadastro")}>Cadastrar</button> */}
+
+                
+                {/* metodo window: apenas paginas internas, mais rapido e é nativa (nao é uma biblioteca) */}
+                {/* metodo window é a melhor opção de acordo com o professor */}
+                <button onClick={() => window.location.href = "/cadastro"}>Cadastrar</button>
+            </div>
+
+
+            <div className="w-[60%] h-full bg-[#6366ee] rounded-l-lg flex flex-col items-center justify-center">
+                <img src={svg} alt="imagem-animada" width={600}/>
+                <p className="text-white text-[20px] text-center font-bold w-[300px] ">A melhor experiencia de login que você ja teve na sua vida!</p>
+            </div>
+        </div>
     )
-
-
 }
